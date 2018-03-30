ded1d1505cf84e844f1a134d90634e1e1e3117c9
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-03-30 11:10:07 -0300

    Aula 5, teste da API

--- test/routes/produto.js
+++ test/routes/produto.js
@@ -1,12 +1,17 @@
 const assert = require('assert')
+const http = require('http') //nativo do node
 
-function soma (n1,n2) {
-    return n1+n2
-}
 
-describe('# Modulo de Soma', function () {
-    it('2 + 2 deve ser 4', () => assert.deepEqual(soma(2,2), 4, "deve retornar 4"))
-
-    it('2 + 3 deve ser 5', () => assert.equal(soma(2,3), 5, "deve retornar 5"))
-
+describe('# API de Produtos', function () {
+    it(
+        'Home com Produtos'
+        , () => {
+        
+            http.get(
+                'http://localhost:3000'
+                , response => assert.equal(response.headers['Content-Type'], 'application/json')
+            )
+            
+        }
+    )
 })
