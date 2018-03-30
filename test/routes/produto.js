f7ba1295a24bfe0617b49871f80a37cbeef6d3e0
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-03-30 11:32:14 -0300

    Aula 5 testes da API

--- test/routes/produto.js
+++ test/routes/produto.js
@@ -1,17 +1,49 @@
-const assert = require('assert')
-const http = require('http') //nativo do node
+const assert = require('assert'),
+      http = require('http') //nativo do node
 
-
+// O Describe e o it server para você modularizar seus testes
 describe('# API de Produtos', function () {
+    
+    const opcoes = {
+        hostname: 'localhost',
+        port: '3000',
+        path: '/',
+    }
+
     it(
-        'Home com Produtos'
-        , () => {
-        
+        'Home com Produtos HTML'
+        , done => {
+            //done é para testes assíncronos    
+            opcoes.headers = {
+                'Accept': 'text/html'
+            }
+
             http.get(
-                'http://localhost:3000'
-                , response => assert.equal(response.headers['Content-Type'], 'application/json')
+                opcoes
+                , response => {
+                    assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+                    done()  
+                }
             )
             
+        }
+    )
+
+    it(
+        'Home com Produtos JSON'
+        , done => {
+            opcoes.headers = {
+                'Accept': 'application/json'
+            }
+
+            http.get(
+                opcoes
+                , response => {
+                    assert.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+                    done()
+                }
+            )
+
         }
     )
 })
