26652ee6a82ecf09c51717b67729f8d1fa21a426
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-03-30 12:05:38 -0300

    Aula 5 testand API com supertest

--- test/routes/produto.js
+++ test/routes/produto.js
@@ -1,49 +1,32 @@
 const assert = require('assert'),
-      http = require('http') //nativo do node
+    http = require('http'), //nativo do node
+    request = require('supertest'), //foca em testar requisicoes http
+    servidor = require('../../servidor')
 
 // O Describe e o it server para você modularizar seus testes
-describe('# API de Produtos', function () {
-    
-    const opcoes = {
-        hostname: 'localhost',
-        port: '3000',
-        path: '/',
-    }
+describe('# API de Produtos', () => {
 
     it(
         'Home com Produtos HTML'
         , done => {
-            //done é para testes assíncronos    
-            opcoes.headers = {
-                'Accept': 'text/html'
-            }
-
-            http.get(
-                opcoes
-                , response => {
-                    assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
-                    done()  
-                }
-            )
-            
+            request(servidor)
+                .get('/')
+                .expect('Content-Type', /html/)
+                .expect(200)
+                .end(done)
         }
     )
 
+    /*
     it(
         'Home com Produtos JSON'
         , done => {
-            opcoes.headers = {
-                'Accept': 'application/json'
-            }
-
-            http.get(
-                opcoes
-                , response => {
-                    assert.equal(response.headers['content-type'], 'application/json; charset=utf-8')
-                    done()
-                }
-            )
-
+            request(servidor)
+                .get('/')
+                .expect('Content-Type', /json/)
+                .expect(200, done)
+                .end(done)
         }
     )
+    */
 })
